0cef2c1ce6a6d9c52952ec3136c51a4b5a530e97
Kacper <kacperkozera13@gmail.com>
2024-04-09 16:44:36 +0200

    first commit

---
+++ src/test/java/FlyAway/FlyAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package FlyAway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
